ec29f26c2e8e675d4c33483966a5cca82071867f
LeKhang-cn22h <chube02082017@gmail.com>
2026-04-22 11:17:23 +0700

    feat: add config ngrok

--- app/config.py
+++ app/config.py
@@ -28,7 +28,8 @@ class Settings(BaseSettings):
     CLOUDINARY_API_URL:        str = ""
 
     DATABASE_URL:            str = ""
-
+    GLB_STATIC_DIR: str = os.path.join(os.path.dirname(os.path.dirname(__file__)), "static", "models")
+    BASE_URL: str = "http://localhost:8000"
     class Config:
         env_file = ".env"
 
